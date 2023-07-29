2c81276a368a09f9e30fd75423b46bc08070bd38
Praveen <rpraveen.engineer@gmail.com>
2023-07-29 13:30:42 +0530

    Added player movement

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -4,19 +4,42 @@ using System;
 public class Player : KinematicBody2D
 {
 
-	private AnimatedSprite animatedSprite;
+	[Export]
+	private int gravity = 800;
+	
+	[Export]
+	private int speed = 100;
+	private int jumpForce = 300;
 
+	private AnimatedSprite animatedSprite;
+	private Vector2 newVelocity = Vector2.Zero;
+	
 	public override void _Ready()
 	{
 		animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
 	}
 
-    public override void _Process(float delta)
-    {
-        if (Input.IsActionJustPressed("move_right")) 
+	public override void _PhysicsProcess(float delta)
+	{
+		if (!IsOnFloor()) 
 		{
-			animatedSprite.Play("run");
+			newVelocity += new Vector2(0, gravity * delta);	
+			if (newVelocity.y > 500) 
+			{
+				newVelocity.y = 500;
+			}
 		}
-    }
+		
+		if (Input.IsActionJustPressed("jump")) 
+		{
+			newVelocity.y = -jumpForce;
+		}
+		
+		var direction = Input.GetAxis("move_left", "move_right");
+		
+		newVelocity.x = direction * speed;
+		
+		newVelocity = MoveAndSlide(newVelocity, Vector2.Up);
+	}
 
 }
