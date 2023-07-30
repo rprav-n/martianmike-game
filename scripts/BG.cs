bbda75818b320e62c770852f6cdf8b613be39b02
Praveen <rpraveen.engineer@gmail.com>
2023-07-30 15:43:16 +0530

    Added scrolling background

---
+++ scripts/BG.cs
@@ -0,0 +1,34 @@
+using Godot;
+using System;
+
+public class BG : ParallaxBackground
+{
+	[Export]
+	private int scrollSpeed = 15;
+	
+	[Export]
+	private Texture bg;
+	
+	private Sprite sprite;
+
+	public override void _Ready()
+	{
+		sprite = GetNode<Sprite>("ParallaxLayer/Sprite"); 
+		sprite.Texture = bg;
+		sprite.RegionEnabled = true;
+	}
+
+	public override void _Process(float delta)
+	{
+		var newPosition = new Vector2(scrollSpeed, scrollSpeed);
+		var rect = sprite.RegionRect;
+		rect.Position += delta * newPosition;
+		
+		if (sprite.RegionRect.Position >= new Vector2(64, 64)) 
+		{
+			rect.Position = Vector2.Zero;	
+		}
+		GD.Print("rect.Position", rect.Position);
+		sprite.RegionRect = rect;
+	}
+}
